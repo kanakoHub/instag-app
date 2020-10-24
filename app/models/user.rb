b77382a4103114f819df7bae7494e899329da07d
user <zhensongjianaizi@gmail.com>
2020-10-24 17:08:35 +0900

    validationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :account, presence: true, uniqueness: true
 end
