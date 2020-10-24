885a8f4acbe79de853d8fcfb769b0073ecb2acf5
user <zhensongjianaizi@gmail.com>
2020-10-24 17:02:13 +0900

    accountカラムの制約変更

--- db/migrate/20201024075255_add_account_to_users.rb
+++ db/migrate/20201024075255_add_account_to_users.rb
@@ -1,6 +1,6 @@
 class AddAccountToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :account, :string
+    add_column :users, :account, :string, default: "", null: false
     add_index :users, :account, unique: true
   end
 end
