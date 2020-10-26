486d613bf0f33efc194dc6683a52797afd8ba8b9
user <zhensongjianaizi@gmail.com>
2020-10-26 23:01:16 +0900

    user登録時にprofileを作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,15 @@ class ProfilesController < ApplicationController
   
   def show
     @user = current_user
+    @profile = @user.profile
   end
 
+  private
+
+  def profile_params
+    params.require(:profile).permit(
+      :account,
+      :avatar
+    )
+  end
 end
