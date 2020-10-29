76cb82be41c85ea063797cc1b85b07be4d61a6ac
user <zhensongjianaizi@gmail.com>
2020-10-30 00:18:18 +0900

    like#showを削除

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,11 +1,4 @@
 class Api::LikesController < Api::ApplicationController
-  # def show
-  #     snap = Snap.find(params[:snap_id])
-  #     like_status = current_user.has_liked?(snap)
-  #     # binding.pry
-  #     render json: { hasLiked: like_status }
-  # end
-
   def create
       snap = Snap.find(params[:snap_id])
       snap.likes.create!(user_id: current_user.id)
